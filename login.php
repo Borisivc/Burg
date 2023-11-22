43af5e9b220523058feb73d0e4eefdc5db6c5dfa
Borisivc <101225512+Borisivc@users.noreply.github.com>
2023-11-22 17:55:11 -0300

    form login + style

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+    <div id="login-container">
+        <form method="post" action="" name="signin-form">
+            <div class="form-element">
+                <label>Email</label>
+                <input type="email" name="correo" required />
+            </div>
+            <div class="form-element">
+                <label>Password</label>
+                <input type="password" name="password" required />
+            </div>
+            <button type="submit" name="login" value="login">Log In</button>
+        </form>
+    </div>
+</body>
+
+</html>
